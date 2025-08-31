dea5124468aea716576bc353bcde5a30fd60b739
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-08-31 13:06:42 -0300

    Implementa Enum com políticas de desconto

--- src/main/java/br/com/mariojp/solid/ocp/CustomerType.java
+++ src/main/java/br/com/mariojp/solid/ocp/CustomerType.java
@@ -1,2 +1,17 @@
 package br.com.mariojp.solid.ocp;
-public enum CustomerType { REGULAR, PREMIUM, PARTNER }
+public enum CustomerType {
+
+    REGULAR( new RegularClass() ),
+    PREMIUM( new PremiumClass() ),
+    PARTNER( new PartnerClass() );
+
+    private DiscountPolicy discountPolicy;
+
+    CustomerType(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    public double apply(double amount) {
+        return discountPolicy.apply(amount);
+    }
+}
